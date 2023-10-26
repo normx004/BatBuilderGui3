3a39ac0372ab978ac97c08ac273af1b886175b46
normx004 <norm.nexon@gmail.com>
2023-10-26 15:16:40 -0500

    css selector for 3840 screen

--- CssSelector.js
+++ CssSelector.js
@@ -10,21 +10,26 @@ function getCss() {
 		 linky.setAttribute('href', 'css/gui-mobile-2-4.css');
 		 console.log("Have initially set css to mobile"); 
 		}
-
-	if ( w > 1366) { 
-	      console.log("Setting css to 1980"); 
-		  linky.setAttribute('href', 'css/gui-1920-2-4.css');
-	   	  console.log("hjave set css to 1980");      
-	  } else {
-		if ( w < 1300) {
-		         console.log("Setting css to tablet");
-			  linky.setAttribute('href', 'css/gui-tablet-2-4.css');
-			   console.log("have set css to tablet");
-		} else  {          
-			     console.log("Setting css to 1366");
-			     linky.setAttribute('href', 'css/gui-1366-2-4.css');
-			     console.log("have set css to 1366");							
-       		    }
+		
+	if ( w > 2000) {	
+         console.log("Setting css to 3840"); 
+		  linky.setAttribute('href', 'css/gui-3824-2-4.css');
+	   	  console.log("hjave set css to 3840");
+	} else {
+		if ( w > 1366) { 
+	    	  console.log("Setting css to 1980"); 
+		  	linky.setAttribute('href', 'css/gui-1920-2-4.css');
+	   	  	console.log("hjave set css to 1980");      
+	  	} else {
+			if ( w < 1300) {
+		    	console.log("Setting css to tablet");
+			  	linky.setAttribute('href', 'css/gui-tablet-2-4.css');
+			   	console.log("have set css to tablet");
+			} else  {          
+			     	console.log("Setting css to 1366");
+			     	linky.setAttribute('href', 'css/gui-1366-2-4.css');
+			     	console.log("have set css to 1366");							
+       		    	}
 	}
    document.head.appendChild(linky); 									   
 }
